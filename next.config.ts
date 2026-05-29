5623f93830208265a58e3f7421cf9bb33d163d88
dpprbot-prog <dpprbot@gmail.com>
2026-05-29 21:27:01 +0300

    chore: move outputFileTracingRoot option to root configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname),
-  },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
+
 
